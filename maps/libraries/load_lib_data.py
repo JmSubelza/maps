e7cdd31db351f8f638f9ee4b67906282d9e3f0e2
Duane Hilton <duanehilton@Duanes-MacBook-Pro.local>
2013-01-26 11:22:53 -0600

    More information from api data now used in map.

--- maps/libraries/load_lib_data.py
+++ maps/libraries/load_lib_data.py
@@ -1,16 +1,37 @@
-import json
-import urllib2
-import sys, os 
+import sys 
+import os 
+
 sys.path.append('/Users/duanehilton/sites/maps')
 sys.path.append('/Users/duanehilton/sites/maps/maps')
 sys.path.append('/Users/duanehilton/sites/maps/maps/libraries')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'maps.maps.settings'
 
+import json
+import urllib2
 from libraries.models import Library 
 
-data = json.load(urllib2.urlopen('http://data.cityofchicago.org/api/views/x8fc-8rcq/rows.json'))
 
-for x in range(len(data['data'])):
-    inst = Library(address=data['data'][x][12], lat=data['data'][x][18][1], lng=data['data'][x][18][2])
-    inst.save()
-    #print '%s, %s, %s' % (data['data'][x][12], data['data'][x][18][1], data['data'][x][18][2])
+class ChiData:
+
+    def __init__(self, url='http://data.cityofchicago.org/api/views/x8fc-8rcq/rows.json'):
+        self.data = json.load(urllib2.urlopen(url))
+
+    def load(self):
+        for x in range(len(self.data['data'])):
+            inst = Library(hours=self.data['data'][x][9], 
+                           location=self.data['data'][x][8], 
+                           address=self.data['data'][x][12], 
+                           url=self.data['data'][x][17][0], 
+                           phone=self.data['data'][x][16], 
+                           lat=self.data['data'][x][18][1], 
+                           lng=self.data['data'][x][18][2])
+            inst.save()
+
+
+def main():
+    dat = ChiData()
+    dat.load()
+
+
+if __name__ == '__main__':
+    main()
--- maps/libraries/models.py
+++ maps/libraries/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 class Library(models.Model):
+    location = models.CharField(max_length=100, null=True, blank=True)
+    address = models.CharField(max_length=100)
+    hours = models.CharField(max_length=250, null=True, blank=True)
+    phone = models.CharField(max_length=100, null=True, blank=True)
+    url = models.CharField(max_length=250, null=True, blank=True)
     lat = models.FloatField()
     lng = models.FloatField()
-    address = models.CharField(max_length=100)
 
